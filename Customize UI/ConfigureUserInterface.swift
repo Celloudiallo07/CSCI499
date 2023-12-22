0b6c62fb0cf06603a6a7bdee28c83d6645e275a5
David Luo <LuoDavidYi@gmail.com>
2023-12-22 10:56:44 -0500

    Update ConfigureUserInterface.swift

--- Customize UI/ConfigureUserInterface.swift
+++ Customize UI/ConfigureUserInterface.swift
@@ -5,12 +5,10 @@ import Foundation
 import UIKit
 
 public class ConfigureUserInterface: NSObject{
-    
     public static func configureNavigationBar(navBar : UINavigationBar){
         navBar.setBackgroundImage(UIImage(), for: .default)
         navBar.shadowImage = UIImage()
         navBar.isTranslucent = true
         navBar.backgroundColor = UIColor.clear
     }
-    
 }
