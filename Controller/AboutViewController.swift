c94e7b4135f55535eba5e5c5bdf869addb8fcf00
David Luo <LuoDavidYi@gmail.com>
2023-12-23 10:27:59 -0500

    Update AboutViewController.swift

--- Controller/AboutViewController.swift
+++ Controller/AboutViewController.swift
@@ -1,19 +1,15 @@
-//
 //  AboutViewController.swift
 //  Meal It
-//
-//
+
 
 import UIKit
 
 class AboutViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
     @IBAction func backButtonAction(){
 //        let settingView = self.storyboard?.instantiateViewController(identifier: SelectController.Storyboard.settingViewController) as? SettingViewController
@@ -24,5 +20,4 @@ class AboutViewController: UIViewController {
 //        self.navigationController?.popViewController(animated: true)
 
     }
-
 }
