8990a11845ca28c78dfe640d44b29763db445f7b
David Luo <LuoDavidYi@gmail.com>
2024-01-12 10:40:19 -0500

    Update CheckHelper.swift

--- Helper/CheckHelper.swift
+++ Helper/CheckHelper.swift
@@ -7,9 +7,7 @@ import Foundation
 import UIKit
 
 class Check {
-    
     static func isPasswordValid(_ password : String) -> Bool {
-        
         let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
         return passwordTest.evaluate(with: password)
     }
